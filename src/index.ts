4dff6164e8e562824552a668e7bc9a7a4fdef02f
Mike Valeriano <mkvlrn@gmail.com>
2024-06-25 13:58:17 -0300

    feat: use top level await in entrypoint

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "~/app.module.ts";
 
-async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-
-await bootstrap();
+const app = await NestFactory.create(AppModule);
+await app.listen(process.env.PORT ?? 3000);
