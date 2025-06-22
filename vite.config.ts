f770c7f6b3388f089f5023f9b202a7198da77f41
Mike Valeriano <mkvlrn@proton.me>
2025-06-22 04:56:10 -0300

    chore: modularize vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,60 +1,13 @@
-import { glob } from "node:fs/promises";
-import { fileURLToPath } from "node:url";
-import { nodeExternals } from "rollup-plugin-node-externals";
 import { defineConfig } from "vite";
+import { getBuild } from "./.vite/build.ts";
+import { getEntryFiles } from "./.vite/entries.ts";
+import { getOptimize } from "./.vite/optimize.ts";
+import { getPlugins } from "./.vite/plugins.ts";
 
-const entryFiles: string[] = [];
-for await (const file of glob("src/**/*.ts")) {
-  // no need to bundle test files
-  if (!(file.endsWith(".test.ts") || file.endsWith(".spec.ts"))) {
-    entryFiles.push(file);
-  }
-}
+const entryFiles = await getEntryFiles();
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      "~": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
-
-  plugins: [
-    nodeExternals({
-      // packages to be set as internal
-      exclude: ["@mkvlrn/result"],
-    }),
-  ],
-
-  build: {
-    target: "esnext",
-    lib: {
-      entry: entryFiles,
-      formats: ["es"],
-      fileName: (_, entryName) => `${entryName}.js`,
-    },
-    outDir: "./build",
-    emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        preserveModules: true,
-        preserveModulesRoot: "src",
-        // this outputs bundled/internal packages to a directory named _internal
-        entryFileNames: ({ name }) => `${name.replace("node_modules/", "_internal/")}.js`,
-      },
-    },
-    minify: "terser",
-    terserOptions: {
-      compress: false,
-      format: {
-        comments: false,
-      },
-    },
-  },
-
-  optimizeDeps: {
-    esbuildOptions: {
-      target: "esnext",
-      format: "esm",
-    },
-  },
+  build: getBuild(entryFiles),
+  plugins: getPlugins(),
+  optimizeDeps: getOptimize(),
 });
