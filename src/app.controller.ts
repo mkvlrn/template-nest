b29e4e449cd8508c8cd711a42fa95bea47326967
Mike Valeriano <mkvlrn@gmail.com>
2025-09-23 17:18:58 -0300

    feat: use injection only for DI

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,11 +3,7 @@ import { type ApiResponse, AppService } from "./app.service.ts";
 
 @Controller("tasks")
 export class AppController {
-  @Inject(AppService) private readonly service: AppService;
-
-  constructor(service: AppService) {
-    this.service = service;
-  }
+  @Inject(AppService) private readonly service!: AppService;
 
   @Get(":id")
   async getTaskById(@Param("id") id: string): Promise<ApiResponse> {
