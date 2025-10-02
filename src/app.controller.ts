e7fc439c33a2f8b6d061464ba40129001b5ae217
Mike Valeriano <mkvlrn@gmail.com>
2025-10-02 18:27:43 -0300

    chore: bump deps

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,7 +3,11 @@ import { type ApiResponse, AppService } from "./app.service.ts";
 
 @Controller("tasks")
 export class AppController {
-  @Inject(AppService) private readonly service!: AppService;
+  private readonly service: AppService;
+
+  constructor(@Inject(AppService) service: AppService) {
+    this.service = service;
+  }
 
   @Get(":id")
   async getTaskById(@Param("id") id: string): Promise<ApiResponse> {
