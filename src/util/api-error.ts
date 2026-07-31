f8b313acc10e114ba4a258d92afe5e1793aefede
mkvlrn <mkvlrn@gmail.com>
2026-07-31 19:07:51 -0300

    chore: use exact dep versions

--- src/util/api-error.ts
+++ src/util/api-error.ts
@@ -1,9 +1,9 @@
-import { defineErrors, type InferAppError } from "@mkvlrn/app-error";
+import { AppError } from "@mkvlrn/app-error";
 
-export const apiError = defineErrors({
+export const apiError = AppError.define({
   resourceNotFound: "NOT_FOUND",
   externalApiError: "BAD_GATEWAY",
   internalApiError: "INTERNAL_SERVER_ERROR",
 });
 
-export type ApiError = InferAppError<typeof apiError>;
+export type ApiError = ReturnType<typeof apiError.create>;
